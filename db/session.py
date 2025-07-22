3e466977a12dc58e443124d3526eb774d274010d
Robert <Robmmar90@gmail.com>
2025-07-21 22:07:19 -0700

    modified:   db/session.py

--- db/session.py
+++ db/session.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import sessionmaker
 
 from db.models import *
 
-# SQLite URL; swap for Postgres/MySQL as needed
 DATABASE_URL = "sqlite:///PybotV2.sqlite3"
 
 engine = create_engine(DATABASE_URL, echo=False, future=True)
