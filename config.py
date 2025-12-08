de157e8fbef82e2bcd10cdd09bde7a12bb6cd752
BuildTools <unconfigured@null.spigotmc.org>
2025-12-08 15:44:06 -0700

    small fixes

--- config.py
+++ config.py
@@ -8,4 +8,6 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 TOKEN = os.getenv("TOKEN")
 ADMIN = os.getenv("ADMIN")
-ALLOWED_CHANNELS = os.getenv("ALLOWED_CH")
+ALLOWED_CHANNELS_IMPORT = os.getenv("ALLOWED_CH")
+
+ALLOWED_CHANNELS = [ch.strip() for ch in ALLOWED_CHANNELS_IMPORT.split(",") if ch.strip()]
